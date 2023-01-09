70d2d2d238d88c79d1000551f14f1f636483103c
Jakub Cee <51480838+JakubCee@users.noreply.github.com>
2023-01-09 15:06:36 +0100

    SQLA supports fast_executemany argument

---
+++ examples/sqla.py
@@ -0,0 +1,32 @@
+import luigi
+from luigi.contrib import sqla
+from datetime import datetime
+
+
+class SQLATask(sqla.CopyToTable):
+    # If database table is already created, then the schema can be loaded
+    # by setting the reflect flag to True
+
+    force = luigi.BoolParameter(default=False)
+    n = luigi.IntParameter(default=10)
+    reflect = True
+    connection_string = "mssql+pyodbc://?odbc_connect=DRIVER={ODBC+Driver+17+for+SQL+Server};SERVER=MSTM1BDB33\DB01;DATABASE=TESTING_DB;Trusted_Connection=yes"  # in memory SQLite database
+    table = "luigi_test"  # name of the table to store data
+    fast_executemany = True
+    echo = False
+
+    def rows(self):
+        for i in range(self.n):
+            yield (i, f"Name_{i}", 5.33 * i, datetime.now() )
+
+    def complete(self):
+        if self.force:
+            return False
+        else:
+            return super().complete()
+
+
+if __name__ == '__main__':
+    task = SQLATask(force=True, n=100000)
+    luigi.build([task], local_scheduler=True)
+
